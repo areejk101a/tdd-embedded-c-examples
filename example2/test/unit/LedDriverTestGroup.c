2b1706da4a5887c4550fe644c304296d9986d683
Arij Kamran <mohammadarijkamran@gmail.com>
2026-07-20 00:35:36 +0500

    feat(example2): first failing test failed

--- example2/test/unit/LedDriverTestGroup.c
+++ example2/test/unit/LedDriverTestGroup.c
@@ -1,4 +1,5 @@
 #include "unity_fixture.h"
+#include "LedDriver.h"
 
 TEST_GROUP_RUNNER(LedDriverTestGroup)
 {
@@ -15,8 +16,11 @@ TEST_TEAR_DOWN(LedDriverTestGroup)
 {
 }
 
-
 TEST(LedDriverTestGroup, LedsOffAfterCreate)
 {
-    TEST_FAIL_MESSAGE("Start here");
+    uint16_t virtualLeds = 0xffff;
+
+    LedDriver_Create( &virtualLeds );
+
+    TEST_ASSERT_EQUAL_HEX16( 0, virtualLeds );
 }
